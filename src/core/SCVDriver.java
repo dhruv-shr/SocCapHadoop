cea457f5de4f5a55353dd2e476be05c398f68e85
dhruv sharma <dhruv.shr@gmail.com>
2013-04-26 22:37:35 -0500

    Adding Build Script and External Dependencies
    + build.xml
    + hadoop jars
    + commons-logging jar
    + changes to the driver

--- src/core/SCVDriver.java
+++ src/core/SCVDriver.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import mapreduce.ForwardPropagationMapper;
 import mapreduce.ForwardPropagationReducer;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -25,7 +26,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
  */
 public class SCVDriver {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		/*
 		 * Arguments list : 1. path of the input file on hdfs. 2. output
 		 * directory on hdfs. 3. number of vertices. (vertex id's should be from
@@ -38,42 +39,59 @@ public class SCVDriver {
 		 * If the above test is passed then the number of input arguments is
 		 * correct and the iterative map reduce job can start.
 		 */
-		String inputPath = args[0];
-		String outputPath = args[1];
+		String path0 = args[0];
+		
 		String intermediateOutputPath = "intermidiate-output/";
-		int numberOfVertices = Integer.parseInt(args[2]);
-		int hopsThreshold = Integer.parseInt(args[3]);
+		int numberOfVertices = Integer.parseInt(args[1]);
+		int hopsThreshold = Integer.parseInt(args[2]);
 
 		/**
 		 * Iterating over each source vertex making it the source for the
 		 * current iteration.
 		 */
-		for (int sourceId = 1; sourceId <= numberOfVertices; sourceId++) {
-			Job iterativeJob = new Job();
+	//	 for (int sourceId = 1; sourceId <= numberOfVertices; sourceId++) {
+			
 			int iteration = 0;
 			/**
 			 * looping for each hop.
 			 */
 			while (iteration <= hopsThreshold) {
-				iterativeJob.setJarByClass(SCVDriver.class);
+				
+				Configuration conf = new Configuration();
+			    conf.set("mapred.job.reduce.input.buffer.percent", "0.7");
+			    conf.set("mapred.inmem.merge.threshold","0");
+			    conf.set("io.sort.factor","95");
+			    conf.set("io.sort.mb","500");
+			    conf.set("mapred.reduce.parallel.copies","30");
+			    conf.set("mapred.map.tasks","5");
+			   // conf.set("mapred.tasktracker.map.tasks.maximum","6");
+			    conf.set("mapred.reduce.tasks",""+ 100); 
+			    conf.set("mapred.compress.map.output", "true");
+			    conf.set("mapred.output.compression.type", "BLOCK"); 
+			    conf.set("mapred.map.output.compression.codec", "org.apache.hadoop.io.compress.GzipCodec");
 				/**
 				 * setting the input path which points to the path of the
 				 * hypergraph file path on the HDFS.
 				 **/
+			    
+			    Job iterativeJob = new Job(conf, "Social Capital");
+			    iterativeJob.setJarByClass(SCVDriver.class);
+			    String inputPath = path0 + "" + iteration;
+			    String outputPath = path0 + "" + (iteration+1);
 				FileInputFormat.addInputPath(iterativeJob, new Path(inputPath));
 				/**
 				 * setting the output path where the intermidiate output will be
 				 * written
 				 * 
 				 */
-				FileOutputFormat.setOutputPath(iterativeJob, new Path(
-						intermediateOutputPath));
+				FileOutputFormat.setOutputPath(iterativeJob,
+						new Path(outputPath));
 				/**
 				 * Setting the source id and the iteration number in the job
 				 * configuration.
 				 */
 				iterativeJob.getConfiguration().setInt(
-						CommonConstants.SOURCE_VERTEX_ID, sourceId);
+						CommonConstants.SOURCE_VERTEX_ID, 1);
 				/******************************************************************/
 				/* SETTING MAPPER CONFIGURATINOS */
 				/******************************************************************/
@@ -101,8 +119,10 @@ public class SCVDriver {
 				iterativeJob.setOutputFormatClass(TextOutputFormat.class);
 				iterativeJob.setOutputKeyClass(IntWritable.class);
 				iterativeJob.setOutputValueClass(VertexValue.class);
+				
+				iterativeJob.waitForCompletion(true);
 			}
 
-		}
+//		}
 	}
 }
